442d4bcd4802faae4b11e54a55bbdd3213bc2db7
ayhamk94 <ayham.kteash1995@gmail.com>
2017-10-04 21:38:42 +0200

    its alive

--- src/index.js
+++ src/index.js
@@ -17,12 +17,57 @@ class Main extends React.Component{
 
   selectBox = (row,col) => {
     let gridCopy = arrayClone(this.state.gridFull);
+    console.log(gridCopy);
     gridCopy[row][col] = !gridCopy[row][col];
     this.setState({
       gridFull:gridCopy
     })
   }
-
+  seed = () => {
+    let gridCopy = arrayClone(this.state.gridFull);
+    for(let i = 0; i<this.rows; i++) {
+      for(let j = 0 ; j< this.cols; j++) {
+        if(Math.floor(Math.random()*4) === 1) {
+          gridCopy[i][j] = true;
+        }
+      }
+    }
+    this.setState(
+      {
+        gridFull:gridCopy
+      }
+    )
+  }
+  play = () => {
+    let g = this.state.gridFull;
+    let g2 = arrayClone(this.state.gridFull);
+    for (let i = 0; i < this.rows; i++) {
+		  for (let j = 0; j < this.cols; j++) {
+		    let count = 0;
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+		    if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+		  }
+		}
+    this.setState({
+      gridFull:g2,
+      generations:this.state.generations + 1
+    })
+  }
+  playButton = () => {
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.play, this.speed);
+  }
+  componentDidMount() {
+    this.seed()
+  }
   render() {
     console.log(this.cols);
     return (
